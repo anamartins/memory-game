9d937f3a1bc65633322109c192812f48946b470f
Ana Martins <anacarolcm@gmail.com>
2020-01-06 22:44:33 +0100

    adding animation and game mechanics

--- script.js
+++ script.js
@@ -1,31 +1,32 @@
 document.addEventListener("DOMContentLoaded", onDOMReady);
 
-let cards = 10;
+let numberCards = 5;
 let level;
 let control=[];
-
+let pair = [];
+let tries = 3;
 
 function onDOMReady() {
     createControlArray();
-    createGrid();
+    createContainer();
     
 }
 
 function createControlArray() {
-    for (let i=0; i<cards; i++) {
+    for (let i=0; i<numberCards; i++) {
         control[i] = 0;
     }
 }
 
 
-function createGrid() {
-    let grid = document.querySelector('.grid');
-    for (let i=0; i<cards*2; i++) {
-        let place = document.createElement('div');
-        place.classList='place';
-        place.dataset.number = i;
-        grid.appendChild(place);
-        place.dataset.card = 'empty';
+function createContainer() {
+    let container = document.querySelector('.container');
+    for (let i=0; i<numberCards*2; i++) {
+        let card = document.createElement('div');
+        card.classList='card';
+        card.dataset.number = i;
+        container.appendChild(card);
+        card.dataset.id = 'empty';
 
         placeCards(i);
     }
@@ -36,25 +37,79 @@ function createGrid() {
 
 
 function placeCards(num) {
-    let place = document.querySelectorAll('.place');
-    let raffle = Math.floor(Math.random() * cards);
+    let card = document.querySelectorAll('.card');
+    let raffle = Math.floor(Math.random() * numberCards);
 
 
     while (control[raffle] === 2) {
-        raffle = Math.floor(Math.random() * cards);
+        raffle = Math.floor(Math.random() * numberCards);
+    }
+
+    let spin = document.createElement('div');
+    spin.className = 'spin';
+    card[num].appendChild(spin);
+    spin.addEventListener('click', onCardClick);
+
+    let cardFront = document.createElement('div');
+    cardFront.className = 'card-front';
+    spin.appendChild(cardFront);
+    let cardImg = document.createElement('img');
+    cardImg.src = `cards/card-${raffle}.svg`;
+    card[num].dataset.id = raffle;
+    cardFront.appendChild(cardImg);
+
+    let cardBack = document.createElement('div');
+    cardBack.className = 'card-back';
+    spin.appendChild(cardBack);
+    let imgBack = document.createElement('img');
+    imgBack.src='cards/back.svg';
+    cardBack.appendChild(imgBack);
+
+    control[raffle] += 1;
+ 
+}
+
+
+function onCardClick() {
+    this.classList.add('spin-click');
+   
+
+    if (pair.length === 0) {
+        pair.push(this.parentNode);
+
+    } else if (pair.length === 1) {
+        pair.push(this.parentNode);
+
+        if (pair[0].dataset.id !== pair[1].dataset.id) {
+            console.log('flip!');
+            setTimeout(flipCards, 1000);
+            tries -= 1;
+            console.log(tries);
+
+            if (tries === 0) {
+                let card = document.querySelectorAll('.card');
+                card[9].style.transform = 'translateX(-800px)';
+                card[9].style.transitionDuration = '2s';
+                
+                console.log('game over!');
+            }
+
+        } else {
+            console.log('yay!');
+            console.log(tries);
+            pair=[];
+
+        }
     }
 
 
+    function flipCards() {
+        for (let i=0; i<pair.length; i++){
+            let div = pair[i].childNodes[0];
+            div.classList.remove('spin-click');
+        }
+        pair=[];
+    }
 
-    let card = document.createElement('img');
-    card.src = `cards/card-${raffle}.svg`;
-    place[num].dataset.card = raffle;
-    place[num].appendChild(card);
-    
-    control[raffle] += 1;
-
-
-
- 
 }
 
